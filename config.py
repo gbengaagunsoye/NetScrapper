ff9c53f6cefc207a9533bd924188559e6b21dcf7
Gbenga Agunsoye <steveso4c@gmail.com>
2020-12-09 10:18:00 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+	#SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
+	#SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db'))
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_TRACK_NOTIFICATIONS = False
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	ADMINS = ['steveso4c@gmail.com']
+	POST_PER_PAGE = 25
